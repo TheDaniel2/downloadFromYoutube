8836ddcb9f335f087b5ca1fb73c63ead00e76dbf
Daniel <dasilvarodriguesdaniel22@gmail.com>
2025-03-18 20:53:11 -0300

    Adicionado widget que contêm UrlField e irá conter o Download Buttom também

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(DownloadFromYoutube());
+  runApp(const DownloadFromYoutube());
 }
 
 class DownloadFromYoutube extends StatelessWidget {
@@ -11,9 +11,56 @@ class DownloadFromYoutube extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //home: 
+      title: 'Download From Youtube',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
+      ),
+      home: HomePage(),
     );
   }
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Builder(
+        builder: (context) {
+          return DownloadField();
+        },
+      ),
+    );
+  }
+}
+
+class DownloadField extends StatelessWidget {
+  const DownloadField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container( //Container me permite mexer no posicionamento e na aparencia
+      color: Colors.white,
+      padding: EdgeInsets.all(10.0),
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+      child: Row(
+        children: [
+          Expanded( //TextField precisa estar dentro de algo como Expanded
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "URL YouTube Video",
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
