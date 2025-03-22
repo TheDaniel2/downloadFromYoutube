ba3bcbfda88d002e67344ce0c2b3e6694285ba83
Daniel <dasilvarodriguesdaniel22@gmail.com>
2025-03-21 22:55:48 -0300

    Widget que exibe os itens da lista, (DownloadsView) no caso as informações acerca dos videos, implementado

--- lib/main.dart
+++ lib/main.dart
@@ -41,9 +41,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Download content from YouTube"),
-      ),
+      appBar: AppBar(title: Text("Download content from YouTube")),
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Column(children: const [DownloadField(), DownloadsView()]),
     );
@@ -59,11 +57,7 @@ class DownloadField extends StatelessWidget {
       color: Colors.white,
       padding: const EdgeInsets.all(10.0),
       margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-      child: Row(children: [
-          UrlField(),
-          DownloadButton(),
-        ],
-      ),
+      child: Row(children: [UrlField(), DownloadButton()]),
     );
   }
 }
@@ -97,7 +91,6 @@ class DownloadButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return ElevatedButton.icon(
       onPressed: () {
         print("...");
@@ -113,11 +106,71 @@ class DownloadsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    //var appState = context.watch<MyAppState>();
 
-    return Text(
-      appState.inputText,
-      style: const TextStyle(fontSize: 18, color: Colors.black),
+    final items = List<ListItem>.generate(
+      1000,
+      (i) => VideoItem('Sender $i', 'Message body $i', "100%"),
+    );
+
+    return Container(
+      color: Colors.white,
+      width: 500,
+      height: 500,
+      padding: const EdgeInsets.all(10.0),
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: ListView.builder(
+        // Let the ListView know how many items it needs to build.
+        itemCount: items.length,
+        // Provide a builder function. This is where the magic happens.
+        // Convert each item into a widget based on the type of item it is.
+        itemBuilder: (context, index) {
+          final item = items[index];
+
+          return Container(
+            color: const Color.fromARGB(255, 187, 225, 255),
+            padding: const EdgeInsets.all(10.0),
+            margin: const EdgeInsets.symmetric(horizontal: 1.0, vertical: 5.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ListTile(
+                    title: item.buildVideoTitle(context),
+                    subtitle: item.buildChannelName(context),
+                  ),
+                ),
+                item.buildDownloadStatus(context)
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
+}
+
+abstract class ListItem {
+  Widget buildVideoTitle(BuildContext context);
+
+  Widget buildChannelName(BuildContext context);
+
+  Widget buildDownloadStatus(BuildContext context);
+}
+
+/// A ListItem that contains data to display a message.
+class VideoItem implements ListItem {
+  final String title;
+  final String channelName;
+  final String downloadStatus;
+
+  VideoItem(this.title, this.channelName, this.downloadStatus);
+
+  @override
+  Widget buildVideoTitle(BuildContext context) => Text(title);
+
+  @override
+  Widget buildChannelName(BuildContext context) => Text(channelName);
+
+  @override 
+  Widget buildDownloadStatus(BuildContext context) => Text(downloadStatus);
 }
